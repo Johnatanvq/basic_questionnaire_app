aa68ef9e62c5a134b4844f2ef3ce4e0f76bfa337
Johnatan Valencia <johnatanv91@gmail.com>
2024-12-28 08:17:02 -0500

    accesing questions object for consuming it

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/material.dart';
+
 import 'package:adv_basics/custom_button.dart';
 import 'package:adv_basics/label_text.dart';
-import 'package:flutter/material.dart';
+import 'package:adv_basics/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -15,16 +17,15 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
 
   @override
   Widget build(context){
+    final currentQuestion = questions[0];
+
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          // const Text(
-          //   'the question'
-          // ),
           LabelText(
-            label: 'the question',
+            label: currentQuestion.text,
             fontSize: 25,
             fontColor: Color.fromARGB(255, 255, 255, 255),
             fontWeight: FontWeight.w500,
@@ -33,21 +34,28 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
             height: 30,
           ),
           CustomButton(
-            text: 'answer 1',
+            text: currentQuestion.answers[0],
             onTap: () {},
           ),
           const SizedBox(
             height: 30,
           ),
           CustomButton(
-            text: 'answer 2',
+            text: currentQuestion.answers[1],
             onTap: () {},
           ),
           const SizedBox(
             height: 30,
           ),
           CustomButton(
-            text: 'answer 3',
+            text: currentQuestion.answers[2],
+            onTap: () {},
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          CustomButton(
+            text: currentQuestion.answers[3],
             onTap: () {},
           ),
         ],
